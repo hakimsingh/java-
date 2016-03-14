c6b658faaa9da774c87a4db27ddd74308948b518
hakimsingh <hakimsinghrajpoot@gmail.com>
2016-03-15 00:50:59 +0530

    box
    read

---
+++ Box.java
@@ -0,0 +1,18 @@
+
+public class Box {
+	int lenght;
+	int breadth;
+	int height;
+	public void SetDimension(int l,int b,int h ){
+		lenght=l;
+		breadth=b;
+		height=h;
+	}
+	public void showdimension(){
+		System.out.println("Lenght="+lenght);
+		System.out.println("Breath="+breadth);
+		System.out.println("Height="+height);
+		
+	}
+
+}
